c9b3f81288785981a13baaf657c35934ec43c606
Santiago Manuel Tamayo Arozamena <145398719+DAMSanti@users.noreply.github.com>
2025-02-05 16:23:23 +0100

    - cambio portrait actividades. Escritorio actividades titulos y padding

--- proyecto_santi/lib/views/activities/views/activitiesLarge_layout_layout.dart
+++ proyecto_santi/lib/views/activities/views/activitiesLarge_layout_layout.dart
@@ -33,39 +33,42 @@ class ActivitiesLargeLandscapeLayout extends StatelessWidget {
             Scaffold(
               body: MarcoDesktop(
                 onToggleTheme: onToggleTheme,
-                content: Column(
-                children: [
-                  Busqueda(
-                    onSearchQueryChanged: (query) {
-                      // Handle search query change
-                    },
-                    onFilterSelected: (filter, date, course, state) {
-                      // Handle filter selection
-                    },
+                content: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column (
+                    children: [
+                      Text("TODAS LAS ACTIVIDADES", style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+                      Busqueda(
+                        onSearchQueryChanged: (query) {
+                          // Handle search query change
+                        },
+                        onFilterSelected: (filter, date, course, state) {
+                          // Handle filter selection
+                        },
+                      ),
+                      Expanded(
+                        child: AllActividades(
+                          selectedFilter: null,
+                          searchQuery: '',
+                          selectedDate: null,
+                          selectedCourse: null,
+                          selectedState: null,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 60.0,
+                        child: Text("TUS ACTIVIDADES", style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+                      ),
+                      Expanded(
+                        child: OtrasActividades(),
+                      ),
+                    ],
                   ),
-                  Expanded(
-                    child: AllActividades(
-                      selectedFilter: null,
-                      searchQuery: '',
-                      selectedDate: null,
-                      selectedCourse: null,
-                      selectedState: null,
-                    ),
-                  ),
-                  Expanded(
-                    child: OtrasActividades(),
-                  ),
-                ],
+                ),
               ),
-            ),
             ),
           ],
         ),
       );
-  }
-
-  bool shouldShowAppBar() {
-    // Implement your logic to show or hide the AppBar
-    return true;
   }
 }
--- proyecto_santi/lib/views/home/views/home_small_landscape_layout.dart
+++ proyecto_santi/lib/views/home/views/home_small_landscape_layout.dart
@@ -21,7 +21,24 @@ class HomeSmallLandscapeLayout extends StatelessWidget {
               child: Column(
                 children: [
                   UserInformation(),
-                  Expanded(
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                  borderRadius: BorderRadius.circular(12.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(-4, -4),
+                      blurRadius: 10.0,
+                      spreadRadius: 1.0,
+                      blurStyle: BlurStyle.inner,
+                    ),
+                  ],
+                ),
+                child: Expanded(
                     child: ListView.builder(
                       itemCount: activities.length,
                       itemBuilder: (context, index) {
@@ -33,6 +50,8 @@ class HomeSmallLandscapeLayout extends StatelessWidget {
                       },
                     ),
                   ),
+              ),
+            ),
                 ],
               ),
             ),
