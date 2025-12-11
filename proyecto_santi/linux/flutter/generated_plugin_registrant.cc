7d1af18b6e436687844f2eee41a54835af4245eb
santiagota <santiagota@T25005>
2025-12-11 12:33:09 +0100

    1

--- proyecto_santi/linux/flutter/generated_plugin_registrant.cc
+++ proyecto_santi/linux/flutter/generated_plugin_registrant.cc
@@ -1,4 +1,22 @@
-﻿void fl_register_plugins(FlPluginRegistry* registry) {
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <audioplayers_linux/audioplayers_linux_plugin.h>
+#include <emoji_picker_flutter/emoji_picker_flutter_plugin.h>
+#include <file_selector_linux/file_selector_plugin.h>
+#include <flutter_secure_storage_linux/flutter_secure_storage_linux_plugin.h>
+#include <printing/printing_plugin.h>
+#include <record_linux/record_linux_plugin.h>
+#include <screen_retriever_linux/screen_retriever_linux_plugin.h>
+#include <url_launcher_linux/url_launcher_plugin.h>
+#include <window_manager/window_manager_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) audioplayers_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "AudioplayersLinuxPlugin");
   audioplayers_linux_plugin_register_with_registrar(audioplayers_linux_registrar);
